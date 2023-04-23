fa21a5d0c003807982ee1b6b7a9e9ae6c1621cd1
YadiraAllauca <yallauca3770@uta.edu.ec>
2023-04-23 01:06:26 -0500

    commit

--- 8puzzlec.py
+++ 8puzzlec.py
@@ -1,67 +1,68 @@
 import random
 
-# Definir la función de fitness
-def fitness(individual):
-    goal = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-    fitness = 0
-    for i in range(len(individual)):
-        if individual[i] == goal[i]:
-            fitness += 1
-    return fitness
+# Calcular la heurística
+def calcular_heuristica(puzzle):
+    estado_objetivo = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    heuristica = 0
+    for i in range(len(puzzle)):
+        if puzzle[i] == estado_objetivo[i]:
+            heuristica += 1
+    return heuristica
 
-# Crear una población inicial aleatoria
-def create_population(population_size):
-    population = []
-    for i in range(population_size):
-        individual = list(range(9))
-        random.shuffle(individual)
-        population.append(individual)
-    return population
+# Población inicial aleatoria
+def generar_poblacion_inicial(tamanio):
+    poblacion = []
+    for i in range(tamanio):
+        puzzle = list(range(9))
+        random.shuffle(puzzle)
+        poblacion.append(puzzle)
+    return poblacion
 
-# Seleccionar individuos para la siguiente generación
-def selection(population, fitness):
-    fitness_sum = sum(fitness)
-    probabilities = [f/fitness_sum for f in fitness]
-    selected_population = []
-    for i in range(len(population)):
-        selected_individual = random.choices(population, weights=probabilities)
-        selected_population.append(selected_individual[0])
-    return selected_population
+# Seleccionar candidatos a padres
+def seleccion(poblacion, heuristica):
+    heuristica_sum = sum(heuristica)
+    probabilidades = [h/heuristica_sum for h in heuristica]
+    poblacion_seleccionada = []
+    for i in range(len(poblacion)):
+        candidato = random.choices(poblacion, weights=probabilidades)
+        poblacion_seleccionada.append(candidato[0])
+    return poblacion_seleccionada
 
-# Crear una nueva generación a partir de la selección y cruce de individuos
-def generate_new_generation(population, fitness):
-    new_generation = []
-    population_size = len(population)
-    selection_size = int(0.5 * population_size) # Seleccionar el 50% de los mejores individuos
-    selected_population = selection(population, fitness)
-    for i in range(selection_size):
-        parent1 = random.choice(selected_population)
-        parent2 = random.choice(selected_population)
-        crossover_point = random.randint(1, 7)
-        child = parent1[:crossover_point] + parent2[crossover_point:]
-        mutation_prob = 0.2 # Probabilidad de mutación
-        if random.random() < mutation_prob:
-            mutation_point1 = random.randint(0, 7)
-            mutation_point2 = random.randint(0, 7)
-            child[mutation_point1], child[mutation_point2] = child[mutation_point2], child[mutation_point1]
-        new_generation.append(child)
-    new_generation += selected_population[:population_size - selection_size]
-    return new_generation
+# Cruzar padres
+def generar_nueva_generacion(poblacion, heuristica):
+    nueva_generacion= []
+    poblacion_tamanio = len(poblacion)
+    tamanio_seleccion = int(0.5 * poblacion_tamanio) # Seleccionar el 50% de los mejores individuos
+    poblacion_seleccionada = seleccion(poblacion,heuristica)
+    for i in range(tamanio_seleccion):
+        padre = random.choice(poblacion_seleccionada)
+        madre = random.choice(poblacion_seleccionada)
+        crossover = random.randint(1, 7)
+        hijo = padre[:crossover] + madre[crossover:]
+        mutacion_prob = 0.15 # Probabilidad de mutación
+        if random.random() < mutacion_prob:
+            mutacion_1 = random.randint(0, 7)
+            mutacion_2 = random.randint(0, 7)
+            hijo[mutacion_1], hijo[mutacion_2] = hijo[mutacion_2], hijo[mutacion_1]
+        nueva_generacion.append(hijo)
+    nueva_generacion += poblacion_seleccionada[:poblacion_tamanio - tamanio_seleccion]
+    return nueva_generacion
 
-# Algoritmo genético principal
-def genetic_algorithm(population_size, max_generations):
-    population = create_population(population_size)
-    for i in range(max_generations):
-        fitness_values = [fitness(individual) for individual in population]
-        max_fitness = max(fitness_values)
-        print(f"Generación {i}, Fitness máximo: {max_fitness}")
-        if max_fitness == 9:
-            print(f"Solución encontrada en la generación {i}!")
-            return population[fitness_values.index(max_fitness)]
-        population = generate_new_generation(population, fitness_values)
+# Solución
+def resolver_puzzle(poblacion_tamanio, num_generaciones):
+    poblacion = generar_poblacion_inicial(poblacion_tamanio)
+    for i in range(num_generaciones):
+        heuristica_val = [calcular_heuristica(puzzle) for puzzle in poblacion]
+        max_heuristica = max(heuristica_val)
+        if max_heuristica == 9:
+            print(f"Solución encontrada en la generación {i}")
+            for j in range(len(poblacion)):
+              print(poblacion[j])
+            return poblacion[heuristica_val.index(max_heuristica)]
+        poblacion = generar_nueva_generacion(poblacion, heuristica_val)
     print("Solución no encontrada.")
     return None
 
-# Ejecutar el algoritmo genético con una población inicial de 100 individuos y un máximo de 100 generaciones
-solution = genetic_algorithm(population_size=100, max_generations=100)
-print("Solución encontrada:", solution)
+# Ejecutar
+solucion= resolver_puzzle(poblacion_tamanio=100, num_generaciones=100)
+print("Solución encontrada:", solucion)

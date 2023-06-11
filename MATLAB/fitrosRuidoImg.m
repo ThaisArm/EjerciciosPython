790edd291aa266257e9f330f9b687c916a8a6af6
YadiraAllauca <94416577+YadiraAllauca@users.noreply.github.com>
2023-06-11 18:22:43 -0500

    Update fitrosRuidoImg.m

--- MATLAB/fitrosRuidoImg.m
+++ MATLAB/fitrosRuidoImg.m
@@ -24,7 +24,7 @@ imshow(imgFilterMean)
 sigma_spatial = 5; % Parámetro de desviación estándar para la similitud espacial
 sigma_range = 0.2; % Parámetro de desviación estándar para la similitud en intensidad
 imgBilateral = imbilatfilt(imgNoise, sigma_spatial, sigma_range);
-figure(6);
+figure(4);
 imshow(imgBilateral);
 %Filtro Gaussiano
 imgGaussian = imgaussfilt(imgNoise);
@@ -32,16 +32,46 @@ figure(5);
 imshow(imgGaussian);
 %Filtro de Wiener
 imgWiener = wiener2(imgNoise);
-figure(7);
+figure(6);
 imshow(imgWiener);
 %Filtro de Mediana
 imgMedian = medfilt2(imgNoise);
-figure(4)
+figure(7)
 imshow(imgMedian)
 
 %FILTRO PROPIO
 
+
 %FFT
+% Aplicar la transformada de Fourier
+imgFFT = fftshift(fft2(imgGray));
 
+% Calcular las dimensiones de la imagen y el centro del espectro
+[m, n] = size(imgGray);
+center = [ceil(m/2), ceil(n/2)];
 
+% Definir el radio del filtro de paso bajo
+radius = 50;
 
+% Crear el filtro de paso bajo en el dominio de la frecuencia
+lowPassFilter = zeros(m, n);
+for i = 1:m
+    for j = 1:n
+        distance = sqrt((i - center(1))^2 + (j - center(2))^2);
+        if distance <= radius
+            lowPassFilter(i, j) = 1;
+        end
+    end
+end
+
+% Aplicar el filtro de paso bajo en el dominio de la frecuencia
+filteredFFT = imgFFT .* lowPassFilter;
+figure(8)
+imshow(filteredFFT)
+
+% Aplicar la transformada inversa de Fourier
+filteredImg = real(ifft2(ifftshift(filteredFFT)));
+
+% Mostrar la imagen filtrada
+figure(9)
+imshow(filteredImg, []);

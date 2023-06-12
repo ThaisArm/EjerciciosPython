2e680ff44c2246bcae52a1b8bf4e3fce6cd3fb0b
YadiraAllauca <94416577+YadiraAllauca@users.noreply.github.com>
2023-06-11 20:46:07 -0500

    Update fitrosRuidoImg.m

--- MATLAB/fitrosRuidoImg.m
+++ MATLAB/fitrosRuidoImg.m
@@ -6,6 +6,7 @@ clear all
 img = imread("Manzana.jpg");
 figure(1)
 imshow(img)
+title("Imagen Original")
 
 %Transformar a escala de grises
 imgGray = rgb2gray(img);
@@ -14,35 +15,38 @@ imgGray = rgb2gray(img);
 imgNoise = imnoise(imgGray, 'salt & pepper', 0.5);
 figure(2)
 imshow(imgNoise)
+title("Imagen con Ruido")
 
 %FILTROS DE MATLAB
-%Filtro de promedio
-imgFilterMean = filter2(fspecial('average',3),imgNoise)/255;
-figure(3)
-imshow(imgFilterMean)
 %Filtro bilateral
 sigma_spatial = 5; % Parámetro de desviación estándar para la similitud espacial
 sigma_range = 0.2; % Parámetro de desviación estándar para la similitud en intensidad
 imgBilateral = imbilatfilt(imgNoise, sigma_spatial, sigma_range);
-figure(4);
+figure(3);
 imshow(imgBilateral);
+title("Filtro Bilateral")
+%Filtro de promedio
+imgFilterMean = filter2(fspecial('average',3),imgNoise)/255;
+figure(4)
+imshow(imgFilterMean)
+title("Filtro de Promedio")
 %Filtro Gaussiano
-imgGaussian = imgaussfilt(imgNoise);
-figure(5);
-imshow(imgGaussian);
-%Filtro de Wiener
-imgWiener = wiener2(imgNoise);
-figure(6);
-imshow(imgWiener);
+sigma = 2; % Desviación estándar
+imgGaussian = imgaussfilt(imgNoise, sigma);
+figure(5)
+imshow(imgGaussian)
+title("Filtro Gaussiano")
 %Filtro de Mediana
 imgMedian = medfilt2(imgNoise);
-figure(7)
+figure(6)
 imshow(imgMedian)
+title("Filtro de Mediana")
 
 %FILTRO PROPIO
 
 
 %FFT
+
 % Aplicar la transformada de Fourier
 imgFFT = fftshift(fft2(imgNoise));
 
@@ -66,12 +70,13 @@ end
 
 % Aplicar el filtro de paso bajo en el dominio de la frecuencia
 filteredFFT = imgFFT .* lowPassFilter;
-figure(8)
+figure(9)
 imshow(filteredFFT)
 
 % Aplicar la transformada inversa de Fourier
 filteredImg = real(ifft2(ifftshift(filteredFFT)));
 
-% Mostrar la imagen filtrada
-figure(9)
-imshow(filteredImg, []);
+% Mostrar la imagen original y la imagen filtrada
+figure(10)
+imshow(filteredImg,[])
+title("Filtro Fourier")
